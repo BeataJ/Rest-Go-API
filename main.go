926f75b5f933dc9c8aed06243df605bdeb7a1bf5
Beata <beata@jasinski.com>
2024-09-06 02:03:47 -0700

    create table

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"example.com/rest-go-api/db"
 	"example.com/rest-go-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
