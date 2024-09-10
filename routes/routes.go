797532cc42b8cfac03b214db52e89b24064e5380
Beata <beata@jasinski.com>
2024-09-09 17:00:31 -0700

    update events

--- routes/routes.go
+++ routes/routes.go
@@ -6,4 +6,5 @@ func RegistetRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvents)
+	server.PUT("/events/:id", updateEvents)
 }
