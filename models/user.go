da1789dec97b9c16f5d0fb954a141720c71f1229
Beata <beata@jasinski.com>
2024-09-17 22:12:48 -0700

    hashing password

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "example.com/rest-go-api/db"
+import (
+	"example.com/rest-go-api/db"
+	"example.com/rest-go-api/utils"
+)
 
 type User struct {
 	ID       int64
@@ -18,7 +21,13 @@ func (u User) Save() error {
 
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassowrd(u.Password)
+
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 
 	if err != nil {
 		return err
